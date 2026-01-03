fd51ad475149ec40019775e8922f5e68216d7bbe
Rafael <bk.rafa@gmail.com>
2026-01-03 20:15:11 -0300

    initial commit

---
+++ main.js
@@ -0,0 +1,30 @@
+const { app, BrowserWindow, session } = require('electron');
+const path = require('path');
+
+async function createWindow() {
+  const customSession = session.fromPartition('persist:whatsapp');
+
+  customSession.setUserAgent('Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36');
+
+  const mainWindow = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      session: customSession
+    }
+  });
+
+  mainWindow.loadURL('https://web.whatsapp.com/');
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', function () {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') app.quit();
+});
